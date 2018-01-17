e7583e6e3dedc3ed7c03c62362fbd266218cae7c
Michael Schwartz <mykesx@gmail.com>
2018-01-17 09:16:58 -0800

    custom editor

---
+++ 10-grid-features/d-custom-editor/src/CustomEditor.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+class CustomEditor extends Component {
+  state = {}
+  componentDidMount () {
+    this.refs.input.addEventListener('blur', this.onBlur);
+    this.focus();
+  }
+  componentDidUpdate () {
+    this.focus();
+  }
+  componentWillUnmount () {
+    this.refs.input.removeEventListener('blur', this.onBlur);
+  }
+  focus () {
+    // focus the input slightly after dbl-click
+    setTimeout(() => {
+      const { refs: { input } } = this;
+      input.focus();
+    });
+  }
+  get value () {
+    let {
+      props,
+      state: { value }
+    } = this;
+    if (value == null) {
+      value = props.value;
+    }
+    return value;
+  }
+  getValue () {
+    return this.value;
+  }
+  onBlur = () => {
+    this.props.stopEditing();
+  }
+  onChange = (event) => {
+    this.setState({
+      value: event.target.value
+    });
+  }
+  render () {
+    return <input
+      ref="input"
+      type="email"
+      value={this.value}
+      onChange={this.onChange}
+      style={{width: "100%"}}
+    />;
+  }
+}
+export default CustomEditor;
+
