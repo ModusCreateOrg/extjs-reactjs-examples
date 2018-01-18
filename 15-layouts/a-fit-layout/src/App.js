728ccf67e62995a231d3bb2f6a42cc4f21157c43
Michael Schwartz <mykesx@gmail.com>
2018-01-18 07:53:35 -0800

    fit layout

---
+++ 15-layouts/a-fit-layout/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const App = (props) => {
+  return (
+    <div className="layout-fit" style={{width: 300, height: 300}}>
+      <div style={{backgroundColor: "red"}}></div>
+    </div>
+  );
+}
+
+
+export default App;
