168f19062a1aaa5979fef275debfb9c073491b9e
Seth Lemmons <seth.lemmons@moduscreate.com>
2018-01-24 11:09:44 -0700

    Synced the Button repo with examples

--- 16-buttons/c-advanced-button/src/App.js
+++ 16-buttons/c-advanced-button/src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
 import { Dropdown } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import './App.css';
 
 const { Divider, Item, Menu } = Dropdown;
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <div className="top-toolbar">
-          <Dropdown
-            text="User Controls"
-            icon="add user"
-            className="icon"
-            floating labeled button
-          >
-            <Menu>
-              <Item onClick={this.handleAddUser}>Add User</Item>
-              <Divider />
-              <Item onClick={this.handleRemoveUser}>Remove User</Item>
-            </Menu>
-          </Dropdown>
-        </div>
+      <div className="top-toolbar">
+        <Dropdown
+          text="User Controls"
+          icon="add user"
+          className="icon"
+          floating labeled button
+        >
+          <Menu>
+            <Item onClick={this.handleAddUser}>Add User</Item>
+            <Divider />
+            <Item onClick={this.handleRemoveUser}>Remove User</Item>
+          </Menu>
+        </Dropdown>
       </div>
     );
   }
--- 16-buttons/d-segmented-button/src/SegmentedButton.js
+++ 16-buttons/d-segmented-button/src/SegmentedButton.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import './SegmentedButton.css';
 
 class SegmentedButton extends Component {
   static defaultProps = {
