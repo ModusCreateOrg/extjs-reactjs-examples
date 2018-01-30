14644e22dc10834586921c396093c42e632e8252
Seth Lemmons <seth.lemmons@moduscreate.com>
2018-01-30 10:52:59 -0700

    Floating Components - updated code and examples Updated code to match what is in the examples in the articles Updated the modal floating example to show on button click

---
+++ 21-floating-components/c-modal/src/Floating.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+// Required to use React portals
+import ReactDOM from 'react-dom';
+import './Floating.css';
+
+class Floating extends Component {
+  state = {
+    show: Boolean(this.props.show)
+  };
+
+  componentWillReceiveProps ({ show }) {
+    this.setState({ show });
+  }
+
+  handleModalClick = () => {
+    this.setState({
+      show: false
+    });
+  };
+
+  render () {
+    const { modal, viewport, closeOnModalClick } = this.props;
+    const { show } = this.state;
+    const isModal = modal ? ' modal' : '';
+    const doShow = show ? ' show' : '';
+    const clsName = `float-wrap${isModal}${doShow}`;
+    const props = {
+      onClick: closeOnModalClick ? this.handleModalClick : null
+    };
+    const float = (
+      <div {...props} className={clsName}>
+        {this.props.children}
+      </div>
+    );
+
+    return viewport ? ReactDOM.createPortal(float, document.body) : float;
+  }
+}
+
+export default Floating;
