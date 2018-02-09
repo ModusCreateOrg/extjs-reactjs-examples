2704d3fddda5c2fd687617a27f277bd13bf6446d
Seth Lemmons <seth.lemmons@moduscreate.com>
2018-02-08 22:00:26 -0700

    Mobx data - added example per the article

---
+++ 27-mobx-data/a-sync-to-async/src/Form.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { action, extendObservable, observable } from 'mobx';
+import { inject, observer } from 'mobx-react';
+import UserStore from './UserStore';
+
+@inject(({ store }, { user }) => {
+  return {
+    store,
+    user: user || (store && store.user)
+  };
+})
+@observer
+class Form extends Component {
+  static defaultProps = {
+    store: new UserStore(),
+    @observable user: {}
+  }
+
+  @action
+  componentWillMount() {
+    const { store, user, userId } = this.props;
+    
+    store.user = user;
+
+    if (store.fetchUser && userId) {
+      store
+        .fetchUser(userId)
+        .then(userData => (store.user = userData))
+        .catch(e => console.log(e));
+    }
+  }
+
+  render() {
+    const { user } = this.props.store;
+
+    return (
+      <form>
+        {this.renderField(user, 'name')}
+        {this.renderField(user, 'email')}
+        {this.renderField(user, 'phone', 'Phone Number')}
+        {this.renderField(user, 'company')}
+        {this.renderField(user, 'department')}
+        {this.renderField(user, 'title')}
+
+        <button onClick={this.submit}>Submit</button>
+        <br />
+      </form>
+    );
+  }
+
+  renderField(user, name, label = name) {
+    if (user[name] == null) {
+      extendObservable(user, {
+        [name]: ''
+      });
+    }
+
+    return (
+      <div style={{ marginBottom: '12px' }}>
+        <label style={{ textTransform: 'capitalize' }}>
+          {label}
+          <input
+            type="text"
+            name={name}
+            value={user[name]}
+            onChange={this.handleFieldChange}
+            style={{ marginLeft: '12px' }}
+          />
+        </label>
+      </div>
+    );
+  }
+
+  @action.bound
+  handleFieldChange(e) {
+    const { onChange, store } = this.props;
+    const { name, value } = e.target;
+
+    store.user[name] = value;
+
+    if (onChange) {
+      onChange({ [name]: value });
+    }
+  }
+
+  submit = e => {
+    e.preventDefault();
+
+    // do submit
+  };
+}
+
+export default Form;
