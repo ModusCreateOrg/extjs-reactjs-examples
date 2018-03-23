a5552cfe57bd761251a17119c395ef9065a6c072
Seth Lemmons <sethlemmons@hotmail.com>
2018-03-23 14:21:25 -0600

    Added data as an import to charts per feedback (#55)

--- 28-charting/f-scatter-chart/src/App.js
+++ 28-charting/f-scatter-chart/src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Legend} from 'recharts';
-
-const data = [
-  {name: 'Page A', uv: 4000, pv: 2400},
-  {name: 'Page B', uv: 3000, pv: 1398},
-  {name: 'Page C', uv: 2000, pv: 9800},
-  {name: 'Page D', uv: 2780, pv: 3908},
-  {name: 'Page E', uv: 1890, pv: 4800},
-  {name: 'Page F', uv: 2390, pv: 3800},
-  {name: 'Page G', uv: 3490, pv: 4300}
-];
+import data from './data';
 
 export default () => {
   return (
